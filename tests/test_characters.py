92394c5a4bad23d1ed0881e2959c4ee3dd0c98bc
Artyom LV <artlv@192.168.1.4>
2021-09-19 22:21:35 +0300

    refactor allure

--- tests/test_characters.py
+++ tests/test_characters.py
@@ -7,8 +7,10 @@ from schema.characters import Character, Characters
 
 @allure.label("layer", "rest")
 @allure.label("owner", "aslagunkov")
-@allure.label("Method", "GET")
 class TestCharacters:
+
+
+    @allure.label("Method", "GET")
     @allure.title("Проверяем структуру ответа на получение списка всех персонажей")
     def test_get_list(self, character_client):
         r = character_client.get_list()
@@ -17,11 +19,13 @@ class TestCharacters:
         with allure.step("Проверяем структуру ответа"):
             Characters.parse_obj(r.json())
 
+    @allure.label("Method", "POST")
     @allure.title("Создаем персонажа с полной информацией")
     def test_create(self, create_character):
         with common_steps.STATUS_200:
             AssertThat(create_character.res.status_code).IsEqualTo(200)
 
+    @allure.label("Method", "POST")
     @allure.title("Создаем персонажа с пустым body")
     def test_create_with_empty_body(self, character_client):
         model = Character()
@@ -29,6 +33,7 @@ class TestCharacters:
         with common_steps.STATUS_400:
             AssertThat(r.status_code).IsEqualTo(400)
 
+    @allure.label("Method", "GET")
     @allure.title("Находим персонажа с помощью имени")
     def test_get_by_name(self, character_client, create_character):
         model = create_character.model  # создаем персонажа
